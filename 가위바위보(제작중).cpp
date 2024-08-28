491033e899a632ff0848a1ab1bfcc0a1a548f630
OscaOsc <oseungchan331@gmail.com>
2024-08-28 12:51:58 +0900

    Add files via upload

---
+++ 가위바위보(제작중).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <stdlib.h> //srand, rand를 사용하기 위한 헤더파일
+int main(void)
+{
+	//srand(time(NULL)); // 난수 초기화
+	char* a;
+	scanf("%s\n",&a);
+	if(a='안녕')
+	{
+		printf("DHSU9DIO") ;
+	}
+}
